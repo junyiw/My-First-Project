e967087e896c152a60dc22cec3de4df42f8e2201
wangjunyi <jwang1@macalester.edu>
2014-02-16 21:50:41 -0600

    homework

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 My-First-Project
 ================
+I am using the Android Developers Tutorial. I have some difficulty following the tutorial now, and
+may need some guidance. I will ask my fellow friends and catch up on learning the material as soon as possible.
--- src/com/example/MyFirstApp/MyActivity.java
+++ src/com/example/MyFirstApp/MyActivity.java
@@ -2,14 +2,27 @@ package com.example.MyFirstApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
